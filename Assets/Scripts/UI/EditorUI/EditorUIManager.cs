f38ee1a2660de8c6408d7dd5d4679692de64f253
ZavodnyPetr <petr.zavodny@sensearena.com>
2022-12-02 07:30:06 +0100

    wip open file dialog

---
+++ Assets/Scripts/UI/EditorUI/EditorUIManager.cs
@@ -0,0 +1,8 @@
+using Scripts.UI.EditorUI;
+using UnityEngine;
+
+public class EditorUIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject body;
+    [SerializeField] private FileOperations fileOperations;
+}
---
+++ Assets/Scripts/UI/EditorUI/FileOperations.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scripts.UI.EditorUI
+{
+    public class FileOperations : MonoBehaviour
+    {
+        [SerializeField] private Button exitButton;
+        [SerializeField] private Button loadButton;
+        [SerializeField] private Button saveButton;
+        [SerializeField] private Button newMapButton;
+    }
+}

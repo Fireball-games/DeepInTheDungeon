8c55c20b681ed265208c0e7bc9336889ce87dc64
ZavodnyPetr <petr.zavodny@sensearena.com>
2023-01-03 05:57:52 +0100

    Founded ToolsWindow, added button to regenerate Guids.

---
+++ Assets/Scripts/Helpers/Editor/ToolsWindow.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Scripts.Building.PrefabsSpawning.Configurations;
+using Scripts.MapEditor;
+using Scripts.System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Helpers.Editor
+{
+    public class ToolsWindow : EditorWindow
+    {
+        [MenuItem("Window/Tools Window")]
+        static void Init()
+        {
+            ToolsWindow window = GetWindow<ToolsWindow>("Tools Window");
+            
+            window.Show();
+        }
+
+        private void OnGUI()
+        {
+            if (!Application.isPlaying) return;
+            
+            if (GameManager.Instance.GameMode == GameManager.EGameMode.Editor)
+            {
+                GUILayout.Label("Map Fixes", EditorStyles.boldLabel);
+            
+                GUILayout.BeginVertical();
+            
+                if (GUILayout.Button("Regenerate GUIDS"))
+                {
+                    bool changesWereMade = false;
+                    foreach (PrefabConfiguration configuration in GameManager.Instance.CurrentMap.PrefabConfigurations)
+                    {
+                        if (string.IsNullOrEmpty(configuration.Guid))
+                        {
+                            changesWereMade = true;
+                            configuration.Guid = Guid.NewGuid().ToString();
+                        }
+                    }
+
+                    if (changesWereMade)
+                    {
+                        MapEditorManager.Instance.SaveMap();
+                    }
+                }
+            
+                GUILayout.EndVertical();
+            }
+        }
+    }
+}

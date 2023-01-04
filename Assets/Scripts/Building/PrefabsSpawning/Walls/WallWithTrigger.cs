53d2039c0372b7ba60f84ce847d6278f4186e806
ZavodnyPetr <petr.zavodny@sensearena.com>
2023-01-04 12:23:34 +0100

    IconStore, new icons, preparing for triggers in editor

---
+++ Assets/Scripts/Building/PrefabsSpawning/Walls/WallWithTrigger.cs
@@ -0,0 +1,9 @@
+﻿using Scripts.Building.PrefabsSpawning.Walls.Identifications;
+
+namespace Scripts.Building.PrefabsSpawning.Walls
+{
+    public class WallWithTrigger : WallPrefabBase, IWallWithTrigger
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UI/IconStore.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconStore : MonoBehaviour
+{
+    [SerializeField] private Sprite move;
+    [SerializeField] private Sprite exclamation;
+    [SerializeField] private Sprite triggerReceiver;
+    [SerializeField] private Sprite wall;
+
+    private static Dictionary<EIcon, Sprite> _sprites;
+
+    public enum EIcon
+    {
+        None = 0,
+        Move = 1,
+        Exclamation = 2,
+        TriggerReceiver = 3,
+        Wall = 4,
+    }
+
+    private void Awake()
+    {
+        _sprites = new Dictionary<EIcon, Sprite>
+        {
+            {EIcon.None, null},
+            {EIcon.Move, wall},
+            {EIcon.Exclamation, exclamation},
+            {EIcon.TriggerReceiver, triggerReceiver},
+            {EIcon.Wall, wall},
+        };
+    }
+
+    public static Sprite Get(EIcon icon) => _sprites[icon];
+}

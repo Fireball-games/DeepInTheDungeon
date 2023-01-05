261cebff55ae20d75ae9691ffeb8690f35a86a38
ZavodnyPetr <petr.zavodny@sensearena.com>
2023-01-05 14:58:23 +0100

    preparation for TriggerEditor, fixed GrateMesh

---
+++ Assets/Scripts/UI/EditorUI/TriggerEditor.cs
@@ -0,0 +1,11 @@
+using Scripts.Helpers;
+using Scripts.System.MonoBases;
+using Scripts.UI.EditorUI.PrefabEditors;
+
+public class TriggerEditor : EditorWindowBase, IPrefabEditor
+{
+    public void CloseWithChangeCheck()
+    {
+        Logger.LogNotImplemented();
+    }
+}
